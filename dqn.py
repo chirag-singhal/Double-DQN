a942559bad08edb4e40ed0f272fdcaf8edc56a0a
chirag-singhal <csinghal208@gmail.com>
2020-05-02 18:32:37 +0530

    Network Architecture

---
+++ dqn.py
@@ -0,0 +1,58 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DQN(nn.Module):
+    
+    def __init__(self, h, w, outputs):
+        """
+            The input to the network is a 84 * 84 * 4 tensor containing a rescaled,
+            and gray scale version of last four frames.
+            
+            The network consists of three convolutional layers followed by two 
+            fully connected layers.
+            
+            Arguments:
+                h: Height of input
+                w: Width of input
+                otputs: Number of total actions possible
+        """
+        
+        super(DQN, self).__init__()
+        
+        self.conv1 == nn.Conv2d(4, 32, kernel_size = 8, stride = 4)
+        self.conv2 == nn.Conv2d(32, 64, kernel_size = 4, stride = 2)
+        self.conv3 == nn.Conv2d(64, 64, kernel_size = 3, stride = 1)
+        
+        def conv_2d_size_output(size, kernel_size, stride):
+            return (size - kernel_size) // stride + 1
+        
+        #Calculating the output size of all the three convolution layers
+        convw1 = conv_2d_size_output(w, 8, 4)
+        convw2 = conv_2d_size_output(convw1, 4, 2)
+        convw3 = conv_2d_size_output(convw2, 3, 1)
+        convh1 = conv_2d_size_output(h, 8, 4)
+        convh2 = conv_2d_size_output(convh1, 4, 2)
+        convh3 = conv_2d_size_output(convh2, 3, 1)
+        linear_input_size = convw3 * convh3 * 64
+        
+        self.fc4 = nn.Linear(linear_input_size, 512)
+        self.fc5 = nn.Linear(512, outputs)
+        
+    def forward(x):
+        """
+            Perform forward propagation of the network. Activation function used is ReLU.
+            
+            Arguments:
+                x: Input to the network
+        """
+        
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        
+        return x
+        
+        
