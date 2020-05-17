91857424240a16b7ad3568b12bb8bcb30d3f269d
Harshan Baskar <harshan01@gmail.com>
2020-05-17 21:12:57 +0530

    Fix frameskip problem

--- input.py
+++ input.py
@@ -7,13 +7,19 @@ import torch.transforms as T
 
 import gym
 
+# The following code can be simplified by using AtariPreprocessing (TODO: Harshan)
+# from gym.wrappers import AtariPreprocessing
+
 
 class Game():
 
     # The transform grayscales and resizes the image to 84x84
+    # Resize resizes to 110x84
+    # Center crop makes it into 84x84
     transform = T.compose([
         T.ToPILImage(),
-        T.Resize((84, 84), interpolation=Image.BILINEAR),
+        T.Resize(84, interpolation=Image.BILINEAR),
+        T.CenterCrop(84),
         T.Grayscale(num_output_channels=1),
         T.ToTensor()])
 
@@ -21,7 +27,13 @@ class Game():
         self.num_frames_per_step = num_frames_per_step
         self.buffer = deque([], self.num_frames_per_step)
         
-        self.env = gym.make(game_name)
+        # Most games in ALE have frameskip.
+        # This makes it difficult to obtain continuous video feed
+        # while performing repeated actions.
+        # For now, environment can be unwrapped and frameskip can
+        # be manually overriden to be 1
+        self.env = gym.make(game_name).unwrapped
+        self.env.frameskip = 1
         self.env.reset()
         
         self.n_actions = self.env.action_space.n
@@ -34,6 +46,7 @@ class Game():
 
     def get_screen():
         # Gym returns screen as HWC numpy array, convert it to CHW
+        # Gym returns (210, 160, 3)
         screen = self.env.render(mode='rgb_array').transpose((2, 0, 1))
 
         # Convert to float, rescale
@@ -59,3 +72,27 @@ class Game():
             if done:
                 break
         return reward, done
+
+
+# Actions in OpenAI Gym ALE
+# -------------------------
+# ACTION_MEANING = {
+#     0: "NOOP",
+#     1: "FIRE",
+#     2: "UP",
+#     3: "RIGHT",
+#     4: "LEFT",
+#     5: "DOWN",
+#     6: "UPRIGHT",
+#     7: "UPLEFT",
+#     8: "DOWNRIGHT",
+#     9: "DOWNLEFT",
+#     10: "UPFIRE",
+#     11: "RIGHTFIRE",
+#     12: "LEFTFIRE",
+#     13: "DOWNFIRE",
+#     14: "UPRIGHTFIRE",
+#     15: "UPLEFTFIRE",
+#     16: "DOWNRIGHTFIRE",
+#     17: "DOWNLEFTFIRE",
+# }
